b04029dd8fd46be3e846f82b5cf0cdacf4f5face
irene_tllo <irene.telloacedo@hotmail.com>
2026-01-16 16:51:53 +0000

    chore: add eslint and prettier configuration

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+export default [
+  {
+    ignores: ["node_modules/**", "dist/**", "build/**", ".expo/**"],
+  },
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      // base recommended-ish
+      ...tsPlugin.configs.recommended.rules,
+
+      // prettier integration
+      ...prettierConfig.rules,
+      "prettier/prettier": "warn",
+
+      // less noise
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
+];
